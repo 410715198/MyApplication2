a981527b08cb54d048c5f70b896a3095cfaf78fc
student <s1071519@gm.pu.edu.tw>
2020-05-15 15:12:42 +0800

    加入master分支程式碼

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView txv = (TextView) findViewById(R.id.txv);
         txv.setText("Hello tcyang");
+        TestMaster();
+    }
 
+    private void TestMaster() {
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setText("Master分支");
     }
 }
