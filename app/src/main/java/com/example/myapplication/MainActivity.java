e74b3e827459368d8b63b77a3d9ab56dc2da9982
student <s1071519@gm.pu.edu.tw>
2020-05-15 15:25:56 +0800

    合併後，再次修改txv顯示的文字

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView txv = (TextView) findViewById(R.id.txv);
-        txv.setText("嗨 我是曾薇瑄");
+        txv.setText("嗨！我是曾薇瑄。");
         TestMaster();
     }
         TestDev();
